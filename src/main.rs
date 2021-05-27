b7378846d2f8e51ffb93e9ad7e6684ae52710813
zyansheep <zyansheep@github.com>
2021-05-27 19:07:28 -0400

    implement player movement

--- src/main.rs
+++ src/main.rs
@@ -51,7 +51,8 @@ fn main() {
 		.add_system_set(
 			SystemSet::on_update(GameState::InGame)
 				.with_system(update_game.system())
-				.with_system(player_movement.system()),
+				.with_system(player_movement.system())
+				.with_system(animate_player.system()),
 		)
 		// Pause Menu
 		.add_system_set(
@@ -73,70 +74,98 @@ struct InGameState {
 	//next_level: Option<Handle<LevelAsset>>,
 	rendered: bool,
 }
+#[derive(Default)]
+struct Player {
+	//player_sprite: Handle<TextureAtlas>,
+	direction: u8, // 0 = up, 1 = down, 2 = left, 3 = right
+}
+
 fn setup_game(
 	mut commands: Commands,
 	asset_server: Res<AssetServer>,
 	mut materials: ResMut<Assets<ColorMaterial>>,
 	mut state: ResMut<InGameState>,
+	mut texture_atlases: ResMut<Assets<TextureAtlas>>,
 ) {
 	commands.spawn_bundle(SpriteBundle {
 		material: materials.add(asset_server.load("backgrounds/creepybackground.png").into()),
 		..Default::default()
 	});
 	state.current_level = asset_server.load::<LevelAsset, _>("game/levels/level0.glevel");
-	asset_server.watch_for_changes().unwrap();
-	/* commands
-	.spawn_bundle(SpriteSheetBundle {
-		texture_atlas: texture_atlas_handle,
-		transform: Transform::from_scale(Vec3::splat(6.0)),
-		..Default::default()
-	})
-	.insert(Timer::from_seconds(0.1, true)); */
 
+	let texture_handle = asset_server.load("sprites/player/player_spritesheet.png");
+	let texture_atlas = TextureAtlas::from_grid(texture_handle, Vec2::new(16.0, 48.0), 4, 4);
+	let texture_atlas_handle = texture_atlases.add(texture_atlas);
+	commands.spawn_bundle(OrthographicCameraBundle::new_2d());
+	commands
+		.spawn_bundle(SpriteSheetBundle {
+			texture_atlas: texture_atlas_handle,
+			transform: Transform::from_scale(Vec3::splat(1.0)),
+			..Default::default()
+		})
+		.insert(Timer::from_seconds(0.3, true))
+		.insert(Player { direction: 0 })
+		.with_children(|commands| {
+			commands.spawn_bundle(OrthographicCameraBundle::new_2d());
+		});
+
+	asset_server.watch_for_changes().unwrap();
 	log::info!("Wake up...");
 }
 
 fn update_game(
 	mut state: ResMut<InGameState>,
 	levels: Res<Assets<LevelAsset>>,
-	mut ev_level: EventReader<AssetEvent<LevelAsset>>
+	mut ev_level: EventReader<AssetEvent<LevelAsset>>,
 ) {
 	for ev in ev_level.iter() {
 		match ev {
 			AssetEvent::Created { handle } | AssetEvent::Modified { handle } => {
-				if let Some(level) = levels.get(handle) {
-					println!("Level Loaded: {:?}", level);
+				let level = levels.get(handle).unwrap();
+				if *handle == state.current_level {
+					println!("Correct Level Loaded: {:?}", level);
 					state.rendered = true;
-				} else {
-					println!("Level not loaded");
 				}
-			},
-			_ => { println!("Level removed"); },
+			}
+			AssetEvent::Removed { .. } => {
+				println!("Level unloaded");
+			}
 		}
 	}
 }
 fn player_movement(
-	mut camera_transform: Query<&mut Transform, With<MainCamera>>,
+	mut player: Query<(&mut Transform, &mut Player, &mut Timer, &mut TextureAtlasSprite)>,
 	keys: Res<Input<KeyCode>>,
-	//mut player: Query<&mut Transform, With<Player>>,
+	time: Res<Time>,
 ) {
-	let mut camera_transform = camera_transform.single_mut().unwrap();
+	let (mut player_transform, mut player, mut player_anim_timer, mut player_sprite) = player.single_mut().unwrap();
 	let speed = 10.0;
 	if keys.pressed(KeyCode::Up) {
-		camera_transform.translation.y += speed;
+		player.direction = 0;
+		player_transform.translation.y += speed;
 	}
 	if keys.pressed(KeyCode::Down) {
-		camera_transform.translation.y -= speed;
+		player.direction = 1;
+		player_transform.translation.y -= speed;
 	}
 	if keys.pressed(KeyCode::Left) {
-		camera_transform.translation.x -= speed;
+		player.direction = 2;
+		player_transform.translation.x -= speed;
 	}
 	if keys.pressed(KeyCode::Right) {
-		camera_transform.translation.x += speed;
+		player.direction = 3;
+		player_transform.translation.x += speed;
+	}
+
+	// Animate Player
+	player_anim_timer.tick(time.delta());
+	if player_anim_timer.finished() {
+		player_sprite.index = 4 * player.direction as u32 + (player_sprite.index + 1) % 4;
+	} else {
+		player_sprite.index = 4 * player.direction as u32 + (player_sprite.index % 4);
 	}
 }
 
-struct MainCamera;
 struct GameStateText;
 fn main_setup(
 	mut commands: Commands,
@@ -144,9 +173,6 @@ fn main_setup(
 	//mut materials: ResMut<Assets<ColorMaterial>>,
 ) {
 	let main_font = asset_server.load("fonts/font.ttf");
-	commands
-		.spawn_bundle(OrthographicCameraBundle::new_2d())
-		.insert(MainCamera);
 	commands.spawn_bundle(UiCameraBundle::default());
 	commands
 		.spawn_bundle(TextBundle {
